af9a57679d573b2fced351e7426385c10b6f64ee
Quasilius-Starlord <joysen833@gmail.com>
2022-03-06 17:37:33 +0530

    mysqlite db

--- droneBackend/settings.py
+++ droneBackend/settings.py
@@ -88,13 +88,17 @@ WSGI_APPLICATION = 'droneBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'droneDatabase',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
-        'USER' : 'postgres',
-        'PASSWORD' : '1234567890'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'droneDatabase',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '5432',
+    #     'USER' : 'postgres',
+    #     'PASSWORD' : '1234567890'
+    # }
 }
 
 
